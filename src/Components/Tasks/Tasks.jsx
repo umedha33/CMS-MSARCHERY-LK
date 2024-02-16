02816b60df9c812027d7f2592132603c5a37ada7
umedha33 <umedharaj@gmail.com>
2024-02-16 15:48:56 +0530

    task table cont.

--- src/Components/Tasks/Tasks.jsx
+++ src/Components/Tasks/Tasks.jsx
@@ -5,10 +5,12 @@ const Tasks = () => {
     return (
         <div>
             <div className="tasks-container">
+
                 <div className="row1-task-header">
                     <h1>Tasks</h1>
                     <button>Assign Task</button>
                 </div>
+                
                 <div className="row2-sort-header">
                     <div className="bread-crum">
                         <h2>All</h2>
@@ -16,39 +18,40 @@ const Tasks = () => {
                         <h2>Completed</h2>
                         <h2>Alerts</h2>
                     </div>
-                    <div className="task-table">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Title</th>
-                                    <th>Description</th>
-                                    <th>Recipient</th>
-                                    <th>Assigned Date</th>
-                                    <th>Due Date</th>
-                                    <th>Status</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>01</td>
-                                    <td>Test Task</td>
-                                    <td>This is a test task</td>
-                                    <td>E Commerce Manager</td>
-                                    <td>2/15/2024</td>
-                                    <td>4/15/2024</td>
-                                    <td><span>Ongoing</span></td>
-                                    <td>
-                                        <span>
+                </div>
+                
+                <div className="row3-task-table">
+                    <table id='table-setting'>
+                        <thead id='table-heading'>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Description</th>
+                                <th>Recipient</th>
+                                <th>Assigned Date</th>
+                                <th>Due Date</th>
+                                <th>Status</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody id='table-elements'>
+                            <tr>
+                                <td>01</td>
+                                <td>Test Task</td>
+                                <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </td>
+                                <td>E Commerce Manager</td>
+                                <td>2/15/2024</td>
+                                <td>4/15/2024</td>
+                                <td><span>Ongoing</span></td>
+                                <td>
+                                    <span>
                                         <i class="fa-solid fa-pen-to-square"></i>
                                         <i class="fa-solid fa-trash"></i>
-                                        </span>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                                    </span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
