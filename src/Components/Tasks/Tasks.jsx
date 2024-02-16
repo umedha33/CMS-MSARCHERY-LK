cce4b06a85e06b68c38fd0a1d04d51cbb9794771
umedha33 <umedharaj@gmail.com>
2024-02-17 00:11:46 +0530

    structure fix

--- src/Components/Tasks/Tasks.jsx
+++ src/Components/Tasks/Tasks.jsx
@@ -10,7 +10,7 @@ const Tasks = () => {
                     <h1>Tasks</h1>
                     <button>Assign Task</button>
                 </div>
-                
+
                 <div className="row2-sort-header">
                     <div className="bread-crum">
                         <h2>All</h2>
@@ -19,7 +19,7 @@ const Tasks = () => {
                         <h2>Alerts</h2>
                     </div>
                 </div>
-                
+
                 <div className="row3-task-table">
                     <table id='table-setting'>
                         <thead id='table-heading'>
@@ -41,12 +41,29 @@ const Tasks = () => {
                                 <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </td>
                                 <td>E Commerce Manager</td>
                                 <td>2/15/2024</td>
-                                <td>4/15/2024</td>
-                                <td><span>Ongoing</span></td>
+                                <td>2/18/2024</td>
+                                <td><span className='status-ico'><i class="fa-solid fa-circle-check"></i></span></td>
                                 <td>
-                                    <span>
-                                        <i class="fa-solid fa-pen-to-square"></i>
-                                        <i class="fa-solid fa-trash"></i>
+                                    <span className='action-btn'>
+                                        <i id='edit-btn' class="fa-solid fa-pen-to-square"></i>
+                                        <br />
+                                        <i id='delete-btn' class="fa-solid fa-trash"></i>
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>02</td>
+                                <td>Aperiam quia autem earum, maiores, non at optio doloribus, accusantium culpa esse sint</td>
+                                <td>Nihil nulla minus at, ab illo laborum tempora praesentium earum totam aspernatur quasi obcaecati</td>
+                                <td>Manager</td>
+                                <td>2/16/2024</td>
+                                <td>2/17/2024</td>
+                                <td><span className='status-ico'><i class="fa-solid fa-bars-progress"></i></span></td>
+                                <td>
+                                    <span className='action-btn'>
+                                        <i id='edit-btn' class="fa-solid fa-pen-to-square"></i>
+                                        <br />
+                                        <i id='delete-btn' class="fa-solid fa-trash"></i>
                                     </span>
                                 </td>
                             </tr>
