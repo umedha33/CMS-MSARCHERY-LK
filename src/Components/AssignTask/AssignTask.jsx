f5194ff9922a927fc587ff7e28e1933ad9c89d07
umedha33 <umedharaj@gmail.com>
2024-02-25 01:16:09 +0530

    assign task form comp

--- src/Components/AssignTask/AssignTask.jsx
+++ src/Components/AssignTask/AssignTask.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './AssignTask.css'
 
 const AssignTask = () => {
+
+    const [files, setFiles] = useState([]);
+
+    const handleFileChange = (event) => {
+        const fileList = event.target.files;
+        const fileArray = Array.from(fileList).map(file =>
+            Object.assign(file, {
+                preview: URL.createObjectURL(file)
+            })
+        );
+        setFiles(prevFiles => [...prevFiles, ...fileArray]); // Concatenate the arrays
+    };
+
+
     return (
         <div className='assign-task-container'>
             <div className="row1-assign-header">
@@ -14,11 +28,68 @@ const AssignTask = () => {
             </div>
 
             <div className="row3-assign-form">
-                <div className="input-elems">
-                    <label>Title</label>
-                    <input type="text" name="title" id="title" placeholder='Task Title' />
-                    <label>Title</label>
-                    <input type="text" name="title" id="title" placeholder='Task Title' />
+
+                <div className="form-container">
+                    <h1>Fill in necessary information to set a task » </h1>
+                    <div className="input-couples">
+                        <label>Task Title:</label>
+                        <input type="text" id='title-text' placeholder='Give a short title for the task' />
+                    </div>
+                    <div className="input-couples">
+                        <label>Task Description:</label>
+                        <textarea name="description" id="description" cols="30" rows="10" placeholder='Provide a clear description about the task in breif'></textarea>
+                    </div>
+                    <div className="input-couples">
+                        <label>Select Recipient:</label>
+                        <select name="recipient" id="recipient">
+                            <option value="" disabled selected hidden>Not Selected</option>
+                            <option value="Manager">Manager</option>
+                            <option value="Accountant">Accountant</option>
+                            <option value="E-Com-Manager">E-Com-Manager</option>
+                            <option value="Sales-Manager">Sales-Manager</option>
+                            <option value="Marketing-Manager">Marketing-Manager</option>
+                            <option value="Content-Creator">Content-Creator</option>
+                            <option value="Customer-Care">Customer-Care</option>
+                            <option value="Logistics-Preperation">Logistics-Preperation</option>
+                        </select>
+                    </div>
+                    <div className="input-couples">
+                        <label>Set Due Date:</label>
+                        <input type="date" id='due-date' />
+                    </div>
+                </div>
+
+                <div className="attachment-container">
+                    <h1>Attachments » </h1>
+                    <div className="input-couples">
+                        <label>Additional Comments:</label>
+                        <textarea name="comment" id="comment" cols="30" rows="10" placeholder='Provide additional comments if required'></textarea>
+                    </div>
+                    <div className="input-couples">
+                        <label>Referral Links:</label>
+                        <textarea name="reflinks" id="reflinks" cols="30" rows="10" placeholder='Provide referral links if available'></textarea>
+                    </div>
+                    <div className="bottom-container">
+                        <label>Images:</label>
+                        <div className="file-input">
+                            <input
+                                type="file"
+                                id="attachment-images"
+                                name="attachment-images"
+                                accept="image/png, image/jpeg"
+                                onChange={handleFileChange}
+                                multiple
+                            />
+                        </div>
+                        <div className="image-preview">
+                            {files.map((file, index) => (
+                                <div key={index} className="thumbnail">
+                                    <img src={file.preview} alt={`Thumbnail ${index}`} />
+                                    <p>{file.name}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
