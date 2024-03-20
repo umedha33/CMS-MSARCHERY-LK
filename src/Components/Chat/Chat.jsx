28f166318495e950b67ecf379cc0aa7cf8faebe1
umedha33 <umedharaj@gmail.com>
2024-03-20 20:00:37 +0530

    chat page basics

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -1,13 +1,69 @@
 import React from 'react'
+import './Chat.css'
+import dpImg from '../Assets/pers2.jpg'
 
 const Chat = () => {
   return (
-    <div>
-      <h1>Chat</h1>
-      <p>a chat section using sockets.</p>
-      <p>should be able to chat with other employees.</p>
-      <p>should be like whatsapp but dedicated to msarchery.</p>
-      <p>file sharing capabilities.</p>
+    <div className='chatapp-container'>
+      <div className="row1-chatapp-header">
+        <h1>MS CHAT</h1>
+        <div className="noti-prof">
+          <i class="fa-solid fa-bell"></i>
+          <img src={dpImg} alt="dp" />
+        </div>
+      </div>
+
+      <div className="row2-divider">
+        <h2>.</h2>
+      </div>
+
+      <div className="chatting-container">
+        <div className="chatlist-container">
+          <div className="searchbar">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input type="text" name="search" id="search" placeholder='Search User' />
+            <i class="fa-solid fa-plus"></i>
+          </div>
+          <div className="chatrender">
+            <div className="chatlist-card">
+              <h2>Sumedha</h2>
+              <p>Hey did you get the contact?</p>
+            </div>
+            <div className="chatlist-card">
+              <h2>Akash</h2>
+              <p>I have the ID</p>
+            </div>
+            <div className="grplist-card">
+              <h2>Logistics Group</h2>
+              <p><span>Lalith: </span>We'll send it...</p>
+            </div>
+            <div className="chatlist-card">
+              <h2>Nihal</h2>
+              <p>Ask for upcoming events</p>
+            </div>
+          </div>
+        </div>
+        <div className="messenger-container">
+          {/* <div className="default-cont">
+            <h2>Select a chat to message...</h2>
+          </div> */}
+
+          <div className="msg-box">
+            <div className="msg-head">
+              <h2>Nihal</h2>
+              <i class="fa-solid fa-circle-info"></i>
+            </div>
+            <div className="msg-body">
+              {/* <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis fuga deserunt quasi aut sapiente inventore quod! Sequi tempore quae hic aspernatur. Aut id officiis illo natus exercitationem, omnis facere delectus!</p> */}
+              <div className="text-sender">
+                <input type="text" name="message-txt" id="message-txt" placeholder='Enter a message' />
+                <i class="fa-solid fa-circle-arrow-right"></i>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
