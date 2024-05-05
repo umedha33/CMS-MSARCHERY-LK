d8c782792b474bc1a9308fee286e1883873901c7
Ethugal Rajaratne <121920950+umedha33@users.noreply.github.com>
2024-05-06 00:23:11 +0530

    Meet struct done

--- Client/src/Components/Meet/Meet.jsx
+++ Client/src/Components/Meet/Meet.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Meet.css'
 import Lobby from '../Lobby/Lobby'
+import ConferenceRoom from '../ConferenceRoom/ConferenceRoom'
 
 const Meet = () => {
   return (
     <div className='meet-container'>
-      <Lobby />
+      {/* <Lobby /> */}
+      <ConferenceRoom />
+
     </div>
   )
 }
